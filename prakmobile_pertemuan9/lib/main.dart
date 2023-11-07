b3f04addfd8966a618dd296b325eca982436d9e0
ilhamshodiq <ilhambheh@gmail.com>
2023-11-08 05:35:51 +0700

    rubah nama folder pertemuan 9

--- prakmobile_pertemuan9/lib/main.dart
+++ prakmobile_pertemuan9/lib/main.dart
@@ -9,7 +9,7 @@ late FirebaseApp app;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   app = await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,);
+    options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
 
